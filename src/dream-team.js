dfe59824656474c50c9fe220b7bdf2dbdfdd3f35
Anton <antonlashyk@gmail.com>
2020-02-16 15:52:25 +0300

    fix: implement correct what-season test

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,24 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let str = "";
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let arr = [];
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] != 'string') {
+      continue;
+    }
+    arr.push(members[i].trim());
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].toUpperCase();
+  }
+  arr = arr.sort();
+  for (let i = 0; i < arr.length; i++) {
+    str += arr[i][0].toUpperCase();
+  }
+  return str;
 };
